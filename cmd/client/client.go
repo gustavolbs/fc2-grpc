f6216f02ba2762332fdc630b385379aadbb2e385
Gustavo Bispo <38411510+gustavolbs@users.noreply.github.com>
2022-06-04 23:40:16 -0300

    add client stream

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/gustavolbs/fc2-grpc/pb"
 	"google.golang.org/grpc"
@@ -22,7 +23,8 @@ func main() {
 	client := pb.NewUserServiceClient(connection)
 
 	// AddUser(client)
-	AddUserVerbose(client)
+	// AddUserVerbose(client)
+	AddUsers(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -67,4 +69,53 @@ func AddUserVerbose(client pb.UserServiceClient) {
 
 		fmt.Println("Status: ", stream.Status, " - ", stream.GetUser())
 	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		{
+			Id: "1",
+			Name: "Gustavo",
+			Email: "1@email.com",
+		},
+		{
+			Id: "2",
+			Name: "Gustavo 2",
+			Email: "2@email.com",
+		},
+		{
+			Id: "3",
+			Name: "Gustavo 3",
+			Email: "3@email.com",
+		},
+		{
+			Id: "4",
+			Name: "Gustavo 4",
+			Email: "4@email.com",
+		},
+		{
+			Id: "5",
+			Name: "Gustavo 5",
+			Email: "5@email.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+
+	if err != nil {
+		log.Fatalf("Error creating Request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
+	}
+
+	fmt.Println(res)
 }
--- services/user.go
+++ services/user.go
@@ -3,6 +3,8 @@ package services
 import (
 	"context"
 	"fmt"
+	"io"
+	"log"
 	"time"
 
 	"github.com/gustavolbs/fc2-grpc/pb"
@@ -12,6 +14,7 @@ import (
 // 	AddUser(context.Context, *User) (*User, error)
 // 	mustEmbedUnimplementedUserServiceServer()
 //  AddUserVerbose(ctx context.Context, in *User, opts ...grpc.CallOption) (UserService_AddUserVerboseClient, error)
+//  AddUsers(ctx context.Context, opts ...grpc.CallOption) (UserService_AddUsersClient, error)
 // }
 
 type UserService struct {
@@ -73,3 +76,29 @@ func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVe
 
 	return nil;
 };
+
+func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
+	users := []*pb.User{}
+
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.Users{
+				User: users,
+			})
+		}
+
+		if err != nil {
+			log.Fatalf("Error receiving stream: %v", err)
+		}
+
+		users = append(users, &pb.User{
+			Id: req.GetId(),
+			Name: req.GetName(),
+			Email: req.GetEmail(),
+		})
+
+		fmt.Println("Adding", req.GetName())
+	}
+}
